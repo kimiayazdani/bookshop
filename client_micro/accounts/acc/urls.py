aa0fe6fcb255fefdd497609e402267772f7061bb
parsa <parsa.eskandar@gmail.com>
2021-04-22 18:58:10 +0430

    Add urls

---
+++ client_micro/accounts/acc/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from .views import *
+
+
+urlpatterns = [
+    path('check_if_account_exists/', does_account_exist_view, name="check_if_account_exists"),
+    path('properties/', account_properties_view, name="properties"),
+    path('update/', update_account_view, name="update"),
+    path('login/', Login.as_view(), name="login"),
+    path('register/', registration_view, name="register"),
+
+
+]
