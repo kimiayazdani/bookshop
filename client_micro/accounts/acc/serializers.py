d714607da1ebb04af544f53ce5430b4e85dbca4d
Kim <yazdanikimia@yahoo.com>
2021-04-22 18:42:28 +0430

    serializers

---
+++ client_micro/accounts/acc/serializers.py
@@ -0,0 +1,50 @@
+import base64
+from django.core.exceptions import ValidationError
+
+from django.contrib.auth.hashers import make_password
+from django.core.files import File
+from rest_framework import serializers
+from django.db import transaction
+
+from .models import Account
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ('username', 'email', 'password', 'phone_number')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(RegistrationSerializer, self).create(validated_data)
+
+
+
+class AccountPropertiesSerializer(serializers.ModelSerializer):
+    avatar = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Account
+        fields = ['pk', 'email', 'bio', 'phone_number', 'username', 'name']
+
+
+class AccountUpdateSerializer(serializers.ModelSerializer):
+    phone_number = serializers.CharField(required=False)
+
+    class Meta:
+        model = Account
+        fields = ['pk', 'phone_number', 'bio', 'name', 'username', 'password']
+        read_only_fields = ['pk']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def update(self, instance, validated_data):
+        if validated_data.get('password', None):
+            validated_data['password'] = make_password(validated_data['password'])
+        return super(AccountUpdateSerializer, self).update(instance, validated_data)
+
+
