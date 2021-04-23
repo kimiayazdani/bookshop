c8c47397f9e135b98fe743b7601f7ead748f56a6
Kim <yazdanikimia@yahoo.com>
2021-04-23 20:51:46 +0430

    login -> admin + client

--- api_gateway/api.py
+++ api_gateway/api.py
@@ -1,7 +1,7 @@
 import json
 
 from flask import Flask, render_template, request, url_for, jsonify
-
+import requests
 
 from http import HTTPStatus
 
@@ -12,8 +12,15 @@ path = "http://127.0.0.1:"
 port_nums = {'admin':8001, 'client':8002, 'search':8003, 'book':8004}
 
 
+@app.route('/client/login/', methods=['POST'])
+def login_client():
+    return requests.post(path+client+'/api/account/login/', json=request.json)
+
+@app.route('/admin/login/', methods=['POST'])
+def login_admin():
+    return requests.post(path+admin+'/api/account/login/', json=request.json)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8004)
+    app.run(debug=True, port=8000)
 
